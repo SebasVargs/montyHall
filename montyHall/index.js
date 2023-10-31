879a8be04e6fa7bea38cefaee28ecf4ef89a06be
SebasVargs <78315075+SebasVargs@users.noreply.github.com>
2023-10-30 19:33:53 -0500

    v1-random

---
+++ montyHall/index.js
@@ -0,0 +1,45 @@
+function randomPosition(arrayLength){
+    const valor = Math.floor(Math.random() * arrayLength)
+    return valor;
+}
+
+function setRandomColors() {
+    const divs = document.querySelectorAll('#randomColor div');
+    const colors = ['blue1', 'blue2'];
+    const randomIndex = randomPosition(divs.length);
+
+    for (let i = 0; i < divs.length; i++) {
+        const randomColorIndex = Math.floor(Math.random() * colors.length);
+        const color = colors[randomColorIndex];
+        divs[i].className = color;
+
+        if (i === randomIndex) {
+            divs[i].className = 'red';
+        }
+    }
+
+    //setRandomImages(randomIndex);
+}
+
+// Llama a la función para establecer los colores aleatorios cuando la página se carga.
+setRandomColors();
+
+function setRandomImages() {
+    const divs = document.querySelectorAll('#randomImages .card-back img');
+    const imagePaths = ['./src/yate.png', './src/yate.png'];
+    const randomIndex = randomPosition(divs.length);
+
+    for (let i = 0; i < divs.length; i++) {
+        const randomImagePath = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+        divs[i].src = randomImagePath;
+
+        if (i === randomIndex) {
+            divs[i].src = './src/toro.png';
+        }
+    }
+
+
+}
+
+// Llama a la función para establecer los colores aleatorios cuando la página se carga.
+setRandomImages();
